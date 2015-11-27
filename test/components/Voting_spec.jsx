2d7c8f54354ca98b3f0ca19f0eb7a8c88d8d218a
root <root@asik-105.hprbd.com>
2015-11-26 23:33:19 -0500

    passing voting onclick form prop

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -2,9 +2,10 @@ import React from 'react/addons'
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
 
-const {renderIntoDocument, scryRenderedDOMComponentsWithTag} = React.addons.TestUtils;
+const {renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate} = React.addons.TestUtils;
 
 describe('Voting', () => {
+
   it('renders a poir of buttons', () => {
     const component = renderIntoDocument(
       <Voting pair={["Trainspotting","28 Days Later"]} />
@@ -13,5 +14,17 @@ describe('Voting', () => {
     expect(buttons.length).to.equal(2);
     expect(buttons[0].textContent).to.equal('Trainspotting');
     expect(buttons[1].textContent).to.equal('28 Days Later');
+  });
+  
+  it('invokes callback when a button is clicked', () => {
+    let votedWith;
+    const vote = (entry) => votedWith = entry;
+    const component = renderIntoDocument(
+      <Voting pair={["Trainspotting", "28 Days Later"]}
+      vote={vote}/>
+    );
+    const buttons = scryRenderedDOMComponentsWithTag(component, 'button');
+    Simulate.click(buttons[0]);
+    expect(votedWith).to.equal('Trainspotting');
   });
 });
